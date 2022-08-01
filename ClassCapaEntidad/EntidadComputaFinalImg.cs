42d32cec47b1ab9b396fb86ffbed87ccb8787054
MichelSanchez290 <michelmiranda3786@gmail.com>
2022-08-01 11:33:44 -0500

    Cambios

---
+++ ClassCapaEntidad/EntidadComputaFinalImg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCapaEntidad
+{
+    public class EntidadComputaFinalImg
+    {
+        public int idimg { get; set; }
+        public string urluno { get; set; }
+        public string urldos { get; set; }
+        public string urltres { get; set; }
+        public string fkcompfinal { get; set; }
+    }
+}
